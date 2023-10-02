873201c58a0751917631a48adee2b52039eecd88
Thunderblarg <Thunderblarg@gmail.com>
2023-10-01 17:51:26 -0700

    Comment cleanup

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ document.addEventListener('DOMContentLoaded', function (){
     let body = document.getElementsByTagName("body")[0];
     body.classList.add("body");
 
-    // let scrolling = anime.timeline({
-    //     loop: true
-    // })
-
     let b1 = new Image();
     b1.src = background;
     b1.classList.add("b1");
@@ -31,14 +27,10 @@ document.addEventListener('DOMContentLoaded', function (){
     .add({  // set scale for layer 1
         targets: b1,
         scale: 2,
-    //    opacity: 1,
-    //    zIndex: 1,
         duration: 0
     }).add({        // set scale for layer 2
         targets: b2,
         scale: 2.5,
-      //  opacity: 1,
-      //  zIndex: 0,
         duration: 0
     }) 
 
@@ -107,149 +99,5 @@ document.addEventListener('DOMContentLoaded', function (){
         duration: 5000,
         easing: 'linear'
     }, '-=5000')
-    
-    
-
-    // let firstT = anime({
-    //     targets: b1,
-    //     translateX: [0, -1100],
-    //     translateY: [0, 390],
-    //     duration: 30000,
-    //     easing: 'linear',
-    //     autoplay: false,
-    //     begin: function(anim){
-    //         anime({
-    //             targets: b1,
-    //             scale: 2,
-    //             duration: 0
-    //         });
-            
-    //         anime({
-    //             targets: b1,
-    //             delay: 25000,
-    //             duration: 5000,
-    //             opacity: [1, 0],
-    //             begin: function(anim) {
-    //                 anime({
-    //                     targets: b2,
-    //                     scale: 3,
-    //                     opacity: 1,
-    //                     duration: 0
-    //                 });
-    //                 secondT.play();
-    //             }                
-    //         })
-    //     },
-    //     complete: function(anim){
-    //         anime({
-    //             targets: b1,
-    //             zIndex: 0,
-    //             duration: 0
-    //         });
-
-    //         anime({
-    //             targets: b2,
-    //             zIndex: 1,
-    //             duration: 0
-    //         });
-    //     }
-    // });
-
-    // let secondT = anime({
-    //     begin: function(anim){
-    //         anime({
-    //             targets: b2,
-    //             delay: 25000,
-    //             duration: 5000,
-    //             opacit{
-    //     targets: b2,
-    //     translateX: [-1100, -400],
-    //     translateY: [-600, -900],
-    //     duration: 30000,
-    //     easing: 'linear',
-    //     autoplay: falsey: [1, 0],
-    //             begin: function(anim) {
-    //                 anime({
-    //                     targets: b1,
-    //                     scale: 2,
-    //                     opacity: 1,
-    //                     duration: 0
-    //                 });
-    //                 firstT.play();
-    //             }                
-    //         })
-    //     },
-    //     complete: function(anim){
-    //         anime({
-    //             targets: b2,
-    //             zIndex: 0,
-    //             duration: 0
-    //         });
-
-    //         anime({
-    //             targets: b1,
-    //             zIndex: 1,
-    //             duration: 0
-    //         });
-    //     }
-    // });
-
-    // firstT.play();
-    
-    
-    // anime({
-    //     targets: b2,
-    //     scale: 3,
-    //     duration: 0,
-    //     translateX: -400,
-    //     translateY: -900,        
-    // });
-    
-
-
-    
-    
-    // scrolling.add({
-    //     targets: body,
-    //     backgroundSize: ['300%', '280%'],
-    //     backgroundPosition: ['12% 25%', '30% 30%'],
-    //     begin: function(anim){
-    //         anime({
-    //             delay: 15000,
-    //         });
-    //     },
-    //     duration: 20000,
-    //     easing: 'linear'
-    // })
-    // .add({
-    //     targets: '.b2',
-    //     duration: 5000,
-    //     easing: 'linear',
-    //     opacity: [0, 1],
-    // }, '-=5000')
-    // .add({
-    //     targets: '.b2',
-    //     translateX : [-400, -1350],
-    //     translateY : [-1150, -800],
-    //     duration: 20000,
-    //     easing: 'linear'    
-    // }, '-=5000')
-    // .add({
-    //     targets: '.b2',
-    //     duration: 5000,
-    //     easing: 'linear',
-    //     opacity: [1, 0],
-    //     begin: function(anim){
-    //         anime({
-    //             targets: body,
-    //             backgroundSize: ['305%', '300%'],
-    //             backgroundPosition: ['7.5% 23.75%', '12% 25%'],
-    //             duration: 5000,
-    //             easing: 'linear'
-    //         })
-    //     },
-    // }, '-=5000')
-
- 
 
 });
