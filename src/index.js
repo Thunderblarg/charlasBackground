d24bf37db1a7e4784a405e0cda56421b3070cee0
Thunderblarg <Thunderblarg@gmail.com>
2023-10-01 17:50:22 -0700

    Better way of going about this, learned a lot about animejs bugs

--- src/index.js
+++ src/index.js
@@ -24,88 +24,86 @@ document.addEventListener('DOMContentLoaded', function (){
 
     let timeline = anime.timeline({
         loop: true
-    })
+    });
 
     // first scan
-
-    timeline.add({
+    timeline
+    .add({  // set scale for layer 1
         targets: b1,
         scale: 2,
-        opacity: 1,
+    //    opacity: 1,
+    //    zIndex: 1,
         duration: 0
-    }).add({
+    }).add({        // set scale for layer 2
         targets: b2,
-        scale: 3,
-        opacity: 1,
+        scale: 2.5,
+      //  opacity: 1,
+      //  zIndex: 0,
         duration: 0
-    }).add({
+    }) 
+
+    .add({                // start scrolling first layer
         targets: b1,
         translateX: [0, -1100],
         translateY: [0, 390],
         duration: 30000,
-        easing: 'linear',
-        begin: function(anim) {
-            anime({
-                targets: b2,
-                scale: 3,
-                opacity: 1,
-                duration: 0
-            });
-        },
-        complete: function(){
-            anime({
-                targets: b1,
-                zIndex: 0,
-                duration: 0
-            });
-
-            anime({
-                targets: b2,
-                zIndex: 1,
-                duration: 0
-            });
-        } 
-    }).add({
+        easing: 'linear'
+    })
+    .add({                // start fading first layer 5s before first scroll ends
         targets: b1,
         duration: 5000,
         easing: 'linear',
-        opacity: [1, 0],        
-    }, '-=5000').add({
-        targets: b2,
-        translateX: [-1100, -400],
-        translateY: [-1300, -1600],
-        duration: 30000,
-        easing: 'linear',
-        begin: function(anim) {
-            anime({
-                targets: b1,
-                scale: 2,
-                opacity: 1,
-                duration: 0
-            });
-        },
-        complete: function(){
-            anime({
-                targets: b2,
-                zIndex: 0,
-                duration: 0
-            });
+        opacity: [1, 0],
+    }, '-=5000')
 
-            anime({
-                targets: b1,
-                zIndex: 1,
-                duration: 0
-            });
-        } 
-    }, '-=5000').add({
+
+    .add({                // do all this JANKY SHIT BECAUSE THE LIBRARY IS BORKED to swap the Z indices
+        targets: b1,
+        zIndex: 0,
+        opacity: 1,
+        duration: 1,
+        offset: '-=1'
+    })
+    .add({
+        targets: b2,
+        zIndex: 1,
+        duration: 1,
+        offset: '-=1'
+    })
+    
+    .add({              // start scrolling second layer
+        targets: b2,
+        translateX: [-1200, -400],
+        translateY: [-1500, -1700],
+        duration: 30000,
+        easing: 'linear'
+    }, '-=5000')
+    .add({             // start fading first layer 5s before first scroll ends
         targets: b2,
         duration: 5000,
         easing: 'linear',
         opacity: [1, 0]
-    }, '-=5000').add({ // this add goes on the end no matter what
+    }, '-=5000')
+
+    .add({                // more jank shit to swap the Z indices again
+        targets: b2,
+        zIndex: 0,
+        opacity: 1,
+        duration: 1,
+        offset: '-=1'
+    })
+    .add({
         targets: b1,
-        translateX: [183, 0],
-        translateY: [-65, 0],
+        zIndex: 1,
+        duration: 1,
+        offset: '-=1'
+    })
+
+
+    .add({ // this add goes on the end no matter what to smoothly cycle back to the first transition
+        targets: b1,
+        translateX: [183, 0], // these values are (x2 - x1 or y2 - y1) / transition time (30s), multiplied by
+        translateY: [-65, 0], // the number of seconds the rollover takes (5s)
         duration: 5000,
         easing: 'linear'
     }, '-=5000')
